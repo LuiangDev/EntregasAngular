a922553e956e3250d00b9a7c798f46b2dc469ff2
Luis Angel <angel.quispe.navarro@outlook.com>
2025-06-01 22:19:03 -0500

    Correction definition

--- EntregableFinal-LuisQuispe/src/app/app.component.ts
+++ EntregableFinal-LuisQuispe/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './layout/navbar/navbar.component';
