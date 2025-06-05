00a86f186279af9052cfa7102d1d363c054811a8
Luis Angel <angel.quispe.navarro@outlook.com>
2025-06-04 23:56:34 -0500

    Add fields of usuarios routing

--- EntregableFinal-LuisQuispe/src/app/app.routes.ts
+++ EntregableFinal-LuisQuispe/src/app/app.routes.ts
@@ -6,32 +6,45 @@ import { RoleGuard } from './auth/role.guard';
 export const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
   },
   {
     path: 'alumnos',
     canActivate: [AuthGuard, RoleGuard],
     data: { expectedRole: 'admin' },
-    loadChildren: () => import('./modules/alumnos/alumnos.module').then(m => m.AlumnosModule)
+    loadChildren: () =>
+      import('./modules/alumnos/alumnos.module').then((m) => m.AlumnosModule),
   },
   {
     path: 'cursos',
     canActivate: [AuthGuard, RoleGuard],
     data: { expectedRole: 'admin' },
-    loadChildren: () => import('./modules/cursos/cursos.module').then(m => m.CursosModule)
+    loadChildren: () =>
+      import('./modules/cursos/cursos.module').then((m) => m.CursosModule),
   },
   {
     path: 'inscripciones',
     canActivate: [AuthGuard],
-    loadChildren: () => import('./modules/inscripciones/inscripciones.module').then(m => m.InscripcionesModule)
+    loadChildren: () =>
+      import('./modules/inscripciones/inscripciones.module').then(
+        (m) => m.InscripcionesModule
+      ),
   },
+  {
+    path: 'usuarios',
+    loadChildren: () =>
+      import('./modules/usuarios/usuarios.module').then(
+        (m) => m.UsuariosModule
+      ),
+  },
+
   {
     path: '',
     redirectTo: 'alumnos',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: '**',
-    redirectTo: 'alumnos'
-  }
+    redirectTo: 'alumnos',
+  },
 ];
