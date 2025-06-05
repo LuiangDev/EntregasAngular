0b99b31500ecb9c334d29ab4e5b78cf4aaa23acd
Luis Angel <angel.quispe.navarro@outlook.com>
2025-06-04 23:53:02 -0500

    Fields of module usuarios

---
+++ EntregableFinal-LuisQuispe/src/app/modules/usuarios/pages/abm-usuarios/abm-usuarios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abm-usuarios',
+  imports: [],
+  templateUrl: './abm-usuarios.component.html',
+  styleUrl: './abm-usuarios.component.scss'
+})
+export class AbmUsuariosComponent {
+
+}
